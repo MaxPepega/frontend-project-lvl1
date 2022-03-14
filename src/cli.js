6361881f9904c4575e604545d45d4d5f93123028
MaximBudko <maximbudko@pop-os.localdomain>
2022-03-14 22:17:33 +0500

    step three project

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+
+import readLineSync from 'readline-sync';
+
+const askName = () =>{
+    const name = readLineSync.question('May I have your name? ');
+    console.log(`Hello, `+name+`!`);
+}
+export default askName;
