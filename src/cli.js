fd636eed028ca7488a92be10cee0a526cb0bc1ec
MaximBudko <maximbudko@pop-os.localdomain>
2022-03-15 22:28:54 +0500

    fourth step

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,7 @@
-
 import readLineSync from 'readline-sync';
 
-const askName = () =>{
-    const name = readLineSync.question('May I have your name? ');
-    console.log(`Hello, `+name+`!`);
-}
+const askName = () => {
+  const name = readLineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
 export default askName;
